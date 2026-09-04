717517bcf78bba84b59711315c6f7338c6b18fc7
Devansh Negi <devansh.negi281@gmail.com>
2026-09-04 18:42:59 +0530

    Add files via upload

---
+++ Q206_Reverse_Linked_List.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    if(head == NULL || head->next == NULL)
+        return head;
+    struct ListNode* prev = NULL;
+    struct ListNode* next = NULL;
+    while (head != NULL)
+    {
+        next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
