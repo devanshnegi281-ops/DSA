47e96b587d46bfe2f6efffa56783d34fb72e4d43
Devansh Negi <devansh.negi281@gmail.com>
2026-09-06 00:00:14 +0530

    Add files via upload

---
+++ Q238_Product_of_Array_Except_Self.c
@@ -0,0 +1,17 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    *returnSize = numsSize;
+    int* result = (int*)malloc(numsSize*sizeof(int));
+    result[0] = 1;
+    for(int i = 1; i < numsSize; i++)
+        result[i] = result[i - 1] * nums[i - 1];
+    int s = 1;
+    for(int i = numsSize - 1; i >= 0; i--)
+    {
+        result[i] *= s;
+        s *= nums[i];
+    }
+    return result;
+}
