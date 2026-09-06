4e2372ddbc0e0e1b3be5f190e9590c239aa61918
Devansh Negi <devansh.negi281@gmail.com>
2026-09-06 21:22:08 +0530

    Add files via upload

---
+++ Q283_Move_Zeroes.c
@@ -0,0 +1,12 @@
+void moveZeroes(int* nums, int numsSize)
+{
+    int l = 0;
+    for (int i = 0; i < numsSize; i++)
+    {
+        if (nums[i] != 0)
+            nums[l++] = nums[i];
+    }
+    while (l < numsSize)
+        nums[l++] = 0;
+    return;
+}
