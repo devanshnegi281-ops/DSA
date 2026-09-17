e790a275cfb6f80c50b033f2ba9dab80d0304a51
Devansh Negi <devansh.negi281@gmail.com>
2026-09-17 22:13:30 +0530

    Add files via upload

---
+++ Q42_Trapping_Rain_Water.c
@@ -0,0 +1,27 @@
+int trap(int* height, int heightSize)
+{
+    int res = 0;
+    if(heightSize < 3)
+        return res;
+    int left = 0, right = heightSize - 1;
+    int leftMax = 0, rightMax = 0;
+    while (left < right){
+        if (height[left] < height[right])
+        {
+            if (height[left] >= leftMax)
+                leftMax = height[left];
+            else
+                res += leftMax - height[left];
+            left++;
+        }
+        else
+        {
+            if (height[right] >= rightMax)
+                rightMax = height[right];
+            else
+                res += rightMax - height[right];
+            right--;
+        }
+    }
+    return res;
+}
