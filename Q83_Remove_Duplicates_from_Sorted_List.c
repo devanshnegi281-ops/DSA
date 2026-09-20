83102be89f80053f52c4e3f768822d2df96f1be5
Devansh Negi <devansh.negi281@gmail.com>
2026-09-20 12:23:36 +0530

    Add files via upload

---
+++ Q83_Remove_Duplicates_from_Sorted_List.c
@@ -0,0 +1,19 @@
+/*
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
+*/
+struct ListNode* deleteDuplicates(struct ListNode* head)
+{
+    struct ListNode* ptr = head;
+    while(ptr != NULL && ptr->next != NULL)
+    {
+        if(ptr->val == ptr->next->val)
+            ptr->next = ptr->next->next;
+        else
+            ptr = ptr->next;
+    }
+    return head;
+}
