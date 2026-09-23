a685b6bc8c0ed28118563bbd2068baeded2c2e56
Devansh Negi <devansh.negi281@gmail.com>
2026-09-23 19:42:10 +0530

    Add files via upload

---
+++ Q70_Climbing_Stairs.c
@@ -0,0 +1,13 @@
+int climbStairs(int n)
+{
+    if(n < 4)
+        return n;
+    int a = 2,b = 3,c;
+    for(int i = 3; i < n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+}
